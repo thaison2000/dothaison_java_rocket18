06ea6cc4c9e9eaec5166b132aac0ee9dbcc22df2
thaison2000 <80248201+thaison2000@users.noreply.github.com>
2021-10-02 14:39:58 +0700

    Create Department.java

---
+++ testing_system2/Department.java
@@ -0,0 +1,32 @@
+package ex5;
+
+public class Department {
+    private int departmentID;
+    private String departmentName;
+   
+    public Department(int departmentID, String departmentName) {
+        this.departmentID = departmentID;
+        this.departmentName = departmentName;
+    }
+
+    public int getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(int departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Department [departmentID=" + departmentID + ", departmentName=" + departmentName + "]";
+    }
+}
