5a1f9f09dada7fd5249f6958ef1b4bca47193928
thaison2000 <80248201+thaison2000@users.noreply.github.com>
2021-10-02 14:36:06 +0700

    Add files via upload

---
+++ testing_system2/Bai1.java
@@ -0,0 +1,19 @@
+package ex5;
+import java.util.Scanner;
+public class Bai1 {
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);  
+        int a1,a2,a3;
+        System.out.println("nhap vao so nguyen thu nhat:");
+        a1 = scanner.nextInt();
+        
+        System.out.println("nhap vao so nguyen thu hai:");
+        a2 = scanner.nextInt();
+    
+        System.out.println("nhap vao so nguyen thu ba:");
+        a3 = scanner.nextInt();
+    
+        
+        }
+}
