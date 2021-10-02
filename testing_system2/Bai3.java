80ef101737b0886ec4303a7dcc386f2f0d6fc769
thaison2000 <80248201+thaison2000@users.noreply.github.com>
2021-10-02 14:38:34 +0700

    Create Bai3.java

---
+++ testing_system2/Bai3.java
@@ -0,0 +1,13 @@
+package ex5;
+
+import java.util.Scanner;
+
+public class Bai3 {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);  
+    String s;
+
+    System.out.println("nhap vao ho ten:");
+    s = scanner.nextLine();
+    }
+}
